8d98841d4b456b6c3ad5936d8efee1e05130b3b1
akshmeh <rishimehta257428@gmail.com>
2024-01-23 18:32:26 +0530

    Test 2

--- src/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import DataReducer from './reducer/DataReducer'
-
-// eslint-disable-next-line react-refresh/only-export-components
-export default configureStore({
-  reducer: {
-    widget: DataReducer,
-  },
-})
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:'/Liquid-MF-Calculator/',
-  plugins: [react()],
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Liquid-MF-Calculator/'
+  }
+
+  return config
 })
